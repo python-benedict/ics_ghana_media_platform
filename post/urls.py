b6409a3386015c8bb74b0dcdd07fe8a7db27281a
Benedict <newlyf38@gmail.com>
2022-10-26 10:07:38 +0000

    made changes to the templates by creating a trending and a trending detail page, als created a treadingPost model in the models.py as well

--- post/urls.py
+++ post/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('<str:id>', views.detail_page, name="detailed_page"),
+    path('trending/', views.trending, name="trending"),
+    path('<str:id>/', views.detail_page, name="detailed_page"),
+    path('<str:id>', views.trending_detailed_page, name="trending_detailed_page")
 ]
--- post/views.py
+++ post/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Tag, Stream, Post, Follow
+from .models import Tag, Stream, Post, Follow, TreandingPost
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
@@ -7,13 +7,24 @@ from django.contrib.auth.models import User
 def home(request):
     posts = Post.objects.all().order_by('-posted')
     context = {
-        'post_items' :  posts
-        
+        'post_items' :  posts   
     }
-
     return render(request, "home.html", context)
+
 
 def detail_page(request, id):
     detailed_page = get_object_or_404(Post, pk=id)
     return render(request, 'detail_page.html', {'detailed_page':detailed_page})
 
+
+def trending(request):
+    trendingposts = TreandingPost.objects.all().order_by('-posted')
+    context = {
+        'trending_items' :  trendingposts   
+    }
+    return render(request, "trending.html", context)
+
+
+def trending_detailed_page(request, id):
+    trending_detail_page = get_object_or_404(TreandingPost, pk=id)
+    return render(request, 'trending_detail_page.html', {'trending_detailed_pages':trending_detail_page})
