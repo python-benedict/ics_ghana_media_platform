342fbc1280ca8129fd084a67eceb2e60191a76ce
Benedict <newlyf38@gmail.com>
2022-10-26 07:52:21 +0000

    added a trendingPost class to the models

--- post/admin.py
+++ post/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from post.models import Tag, Post, Follow, Stream
+from post.models import Tag, Post, Follow, Stream, TreandingPost
 
 # Register your models here.
 
 admin.site.register(Tag)
 admin.site.register(Post)
+admin.site.register(TreandingPost)
 admin.site.register(Follow)
 admin.site.register(Stream)
--- post/models.py
+++ post/models.py
@@ -51,6 +51,25 @@ class Post(models.Model):
     def __str__(self):
         return self.caption
     
+ #if there is an error come and delete this   
+class TreandingPost(models.Model):
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4)
+    title = models.CharField(max_length=500, verbose_name="title", blank=True)
+    picture = models.ImageField(upload_to=user_directory_path, verbose_name="Picture", null=True)
+    caption = models.CharField(max_length=500000, verbose_name="Caption")
+    posted = models.DateTimeField(auto_now_add=True)
+    tag = models.ManyToManyField(Tag, related_name="trendingPostTag")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+    
+    #this will help you get all the post associated with the id
+    def get_absolute_url(self):
+        return reverse('post-details', args=[str(self.id)])
+    
+    def __str__(self):
+        return self.caption    
+
+    
 class Follow(models.Model):
     follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
     following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
